ddc2f394046cb120071bd1d4e979a351312bd3f2
Al-AminDolar <alamin.dolar1@gmail.com>
2023-06-21 22:02:47 +0600

    project done

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const wrapper = document.querySelector(".wrpper");
 const loginLink = document.querySelector(".login-link");
 const registerLInk = document.querySelector(".register-link");
+const btnLOginPopUp = document.querySelector(".login-popup");
+const closeIcon = document.querySelector(".icon-close");
 
 registerLInk.addEventListener("click", () => {
   wrapper.classList.add("active");
@@ -8,4 +10,12 @@ registerLInk.addEventListener("click", () => {
 
 loginLink.addEventListener("click", () => {
   wrapper.classList.remove("active");
+});
+
+btnLOginPopUp.addEventListener("click", () => {
+  wrapper.classList.add("active-popup");
+});
+
+closeIcon.addEventListener("click", () => {
+  wrapper.classList.remove("active-popup");
 });
